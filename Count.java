5a11e3ff786a38852c5dd560621764921d126d09
Mrunal Ahire <mrunal.ahire@gmail.com>
2019-01-19 12:43:18 +0530

    Displays graph for static values

---
+++ Count.java
@@ -0,0 +1,156 @@
+public class Count{
+    static int cnt16,cnt17,cnt18,cnt19,cnt20,cnt21,cnt22,cnt23,cnt24,cnt25,cnt26,cnt27,cnt28,cnt29,cnt30,cnt31,cn
+            ,cnt32,cnt33,cnt34,cnt35,cnt36,cnt37;
+    public static void main(String args[]){
+        double[] arr={24.97,
+            30.17,
+            33.63,
+            24.91,
+            30.95,
+            24.73,
+            28.34,
+            22.57,
+            24.48,
+            26.67,
+            22.04,
+            20.84,
+            19.36,
+            27,
+            25.8,
+            26.69,
+            22.15,
+            22.92,
+            22.17,
+            35.4,
+            23.77,
+            24.91,
+            24.18,
+            24.92,
+            26.26,
+            22.81,
+            24.9,
+            29.24,
+            27.27,
+            25.59,
+            30.54,
+            23.45,
+            22.94,
+            25.72,
+            24.19,
+            22.25,
+            34.96,
+            25.85,
+            23.83,
+            24.19,
+            26.85,
+            26.08,
+            24.58,
+            26.39,
+            24.93,
+            26.73,
+            29.91,
+            33.12,
+            32.2,
+            23.50,
+            25.08,
+            25.43,
+            20.51,
+            25.21,
+            22.14,
+            22.51,
+            23.15,
+            24.98,
+            23.16,
+            37.2,
+            21.76,
+            23.75,
+            20.46,
+            25.44,
+            25.73,
+            23.64,
+            24.08,
+            23.58,
+            27.37,
+            23.72,
+            26.32,
+            23.28,
+            22.67,
+            24.74,
+            21.81,
+            24.07,
+            26.4,
+            23.15,
+            21.05,
+            24.5,
+            25.91,
+            26.46,
+            25.01,
+            24.47,
+            27.67,
+            24.73,
+            20.37,
+            25.31,
+            25.19,
+            28.23,
+            25.73,
+            21.92,
+            30.79,
+            30.47,
+            21.87,
+            25.09,
+            29.13,
+            23.17,
+            21.05,
+            27.13,
+            21.95};
+            
+        for(int i=0;i<101;i++){
+            switch((int)arr[i]){
+                case 16:cnt16++;break;
+                case 17:cnt17++;break;
+                case 18:cnt18++;break;
+                case 19:cnt19++;break;
+                case 20:cnt20++;break;
+                case 21:cnt21++;break;
+                case 22:cnt22++;break;
+                case 23:cnt23++;break;
+                case 24:cnt24++;break;
+                case 25:cnt25++;break;
+                case 26:cnt26++;break;
+                case 27:cnt27++;break;
+                case 28:cnt28++;break;
+                case 29:cnt29++;break;
+                case 30:cnt30++;break;
+                case 31:cnt31++;break;
+                case 32:cnt32++;break;
+                case 33:cnt33++;break;
+                case 34:cnt34++;break;
+                case 35:cnt35++;break;
+                case 36:cnt36++;break;
+                case 37:cnt37++;break;
+            }
+        }
+        System.out.println("16 = "+cnt16);
+        System.out.println("17 = "+cnt17);
+        System.out.println("18 = "+cnt18);
+        System.out.println("19 = "+cnt19);
+        System.out.println("20 = "+cnt20);
+        System.out.println("21 = "+cnt21);
+        System.out.println("22 = "+cnt22);
+        System.out.println("23 = "+cnt23);
+        System.out.println("24 = "+cnt24);
+        System.out.println("25 = "+cnt25);
+        System.out.println("26 = "+cnt26);
+        System.out.println("27 = "+cnt27);
+        System.out.println("28 = "+cnt28);
+        System.out.println("29 = "+cnt29);
+        System.out.println("30 = "+cnt30);
+        System.out.println("31 = "+cnt31);
+        System.out.println("32 = "+cnt32);
+        System.out.println("33 = "+cnt33);
+        System.out.println("34 = "+cnt34);
+        System.out.println("35 = "+cnt35);
+        System.out.println("36 = "+cnt36);
+        System.out.println("37 = "+cnt37);
+    }
+}
